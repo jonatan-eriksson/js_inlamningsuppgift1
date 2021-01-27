759e50ca6d18f3c365dc9f5221fde598ac5a23c5
Jonatan Eriksson <jonatan-eriksson@users.noreply.github.com>
2021-01-27 18:29:31 +0100

    Add edit/undo buttons for blog posts to enable text editing

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
 TODO:
 *! skapa blogginlägg
-* redigera blogginlägg
+*! redigera blogginlägg
 */
 
 "use strict";
@@ -14,23 +14,45 @@ window.onload = () => {
     let posts = createMultiplePosts(e.currentTarget.value);
     appendPosts(posts);
   });
+
+  const editBtn = document.querySelector(".post-edit");
+  editBtn.addEventListener("click", editPost);
 };
 
+// Skapar ett blogginlägg med titel och brödtext och returnerar artikeln
 function createPost(num) {
   const post = document.createElement("article");
   const title = document.createElement("h2");
-  const para = document.createElement("p");
+  const text = document.createElement("p");
+  const editBtn = document.createElement("button");
+  const undoBtn = document.createElement("button");
 
   post.className = "post";
-  title.innerHTML = "Article " + num;
-  para.innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta fugit natus ex eos nesciunt accusantium doloremque architecto. Cumque atque fuga deserunt aspernatur porro culpa consequuntur. Minima aliquam placeat perferendis ad.";
+
+  title.className = "post-title";
+  title.textContent = "Article " + num;
+
+  text.className = "post-text";
+  text.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta fugit natus ex eos nesciunt accusantium doloremque architecto. Cumque atque fuga deserunt aspernatur porro culpa consequuntur. Minima aliquam placeat perferendis ad.";
+
+  editBtn.className = "post-btn post-edit";
+  editBtn.type = "button";
+  editBtn.textContent = "Edit";
+  editBtn.onclick = editPost;
+
+  undoBtn.className = "post-btn post-undo hide";
+  undoBtn.type = "button";
+  undoBtn.textContent = "Undo";
 
   post.append(title);
-  post.append(para);
+  post.append(text);
+  post.append(editBtn);
+  post.append(undoBtn);
 
   return post;
 }
 
+// Skapar flera blogginlägg och lägger dem i en array
 function createMultiplePosts(num) {
   let posts = [];
   for (let i = 1; i <= num; i++) {
@@ -39,17 +61,51 @@ function createMultiplePosts(num) {
   return posts;
 }
 
-function appendPost(post) {
-  const blogPosts = document.querySelector(".blog-posts");
-  blogPosts.append(post);
-}
-
+// Lägger till bloginlägg i elementet med klassen blog-posts
 function appendPosts(posts) {
   const blogPosts = document.querySelector(".blog-posts");
-  posts.forEach((post) => blogPosts.append(post));
+
+  if (Array.isArray(posts)) {
+    posts.forEach((post) => blogPosts.append(post));
+  } else {
+    blogPosts.append(posts);
+  }
 }
 
 function clearPosts() {
   const blogPosts = document.querySelector(".blog-posts");
   blogPosts.innerHTML = "";
+}
+
+function editPost(e) {
+  const editBtn = e.currentTarget;
+  const post = e.currentTarget.parentNode;
+
+  const title = post.querySelector(".post-title");
+  const text = post.querySelector(".post-text");
+  const undoBtn = post.querySelector(".post-undo");
+
+  undoBtn.classList.toggle("hide");
+
+  if (title.isContentEditable) {
+    editBtn.textContent = "Edit";
+    title.contentEditable = false;
+    text.contentEditable = false;
+
+    undoBtn.onclick = "";
+  } else {
+    editBtn.textContent = "Save";
+    title.contentEditable = true;
+    text.contentEditable = true;
+
+    // Spara original titel och text
+    let oldTitle = title.innerHTML;
+    let oldText = text.innerHTML;
+
+    undoBtn.onclick = undoBtn.onclick = () => {
+      // Återställ om man klickar på Undo
+      title.innerHTML = oldTitle;
+      text.innerHTML = oldText;
+    };
+  }
 }
