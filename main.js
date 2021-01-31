1f302e5dc7623745617c4dcc76db5843c852aba8
Jonatan Eriksson <jonatan-eriksson@users.noreply.github.com>
2021-01-31 22:15:42 +0100

    Add sidebar functions to auto create links

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-/*
-TODO:
-*! skapa blogginlägg
-*! redigera blogginlägg
-*/
-
 "use strict";
 
 window.addEventListener("DOMContentLoaded", (e) => {
   const blog = document.querySelector(".blog-posts");
+  const sidebarList = document.querySelector(".sidebar .nav-list");
   const input = document.querySelector("#input-number");
 
-  /* När inputrutan förändras så körs koden, 
-     först töms blog-posts elementet,
-     sen skapas ett antal blogginlägg, lika många som värdet i inputrutan,
-     till sist läggs blogginläggen till i DOMen 
-  */
+  // När input elementet förändras genom att användaren fyller i ett nummer så körs koden
   input.addEventListener("change", (e) => {
-    clearPosts(blog);
-    let posts = createMultiplePosts(e.currentTarget.value);
-    appendPosts(blog, posts);
+    clear(blog); // Rensar blogginläggen
+    let posts = createMultiplePosts(e.currentTarget.value); // Skapar blogginlägg
+    appendPosts(blog, posts); // Lägger till dem i DOMen
+
+    clear(sidebarList); // Rensar sidebarlistan
+    let items = createSidebarItems(posts); // Skapar nya länkar till blogginläggen
+    appendPosts(sidebarList, items); // Lägger till dem i sidebaren
   });
 
-  const editBtn = document.querySelector(".post-edit");
-  editBtn.addEventListener("click", editPost);
+  // Sidebarknappen
+  const sidebarToggle = document.querySelector(".sidebar-toggle");
+  sidebarToggle.onclick = toggleSidebar;
 });
+
+// Tar bort alla blogginlägg
+function clear(element) {
+  element.innerHTML = "";
+}
 
 // Skapar ett blogginlägg med titel och brödtext och returnerar blogginlägget
 function createPost(num) {
@@ -37,6 +38,7 @@ function createPost(num) {
 
   title.className = "post-title";
   title.textContent = "Article " + num;
+  title.id = "article" + num;
 
   text.className = "post-text";
   text.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta fugit natus ex eos nesciunt accusantium doloremque architecto. Cumque atque fuga deserunt aspernatur porro culpa consequuntur. Minima aliquam placeat perferendis ad.";
@@ -76,11 +78,6 @@ function appendPosts(elem, posts) {
   }
 }
 
-// Tar bort allt innuti elementet, för att rensa blogginlägg
-function clearPosts(elem) {
-  elem.innerHTML = "";
-}
-
 function editPost(e) {
   const editBtn = e.currentTarget;
   const post = editBtn.parentNode;
@@ -97,6 +94,7 @@ function editPost(e) {
     title.contentEditable = false;
     text.contentEditable = false;
 
+    updateSidebarItem(title.id, title.innerHTML);
     undoBtn.onclick = "";
   } else {
     // Aktivera redigering på text och titel
@@ -115,4 +113,45 @@ function editPost(e) {
       text.innerHTML = oldText;
     };
   }
+}
+
+// Sidebar
+function toggleSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("collapse");
+}
+
+function updateSidebarItem(id, newText) {
+  const sidebar = document.querySelector(".sidebar .nav-list");
+  let title = sidebar.querySelector("[href='#" + id + "']");
+
+  title.innerHTML = newText;
+}
+
+function createSidebarItem(id, text) {
+  const item = document.createElement("li");
+  const link = document.createElement("a");
+
+  item.className = "nav-item";
+
+  link.className = "nav-link";
+  link.href = "#" + id;
+  link.textContent = text;
+
+  item.append(link);
+
+  return item;
+}
+
+// Skapar en länk till varje blogginlägg
+function createSidebarItems(posts) {
+  let items = [];
+  if (Array.isArray(posts)) {
+    posts.forEach((post) => {
+      let id = post.querySelector(".post-title").id;
+      let title = post.querySelector(".post-title").textContent;
+      items.push(createSidebarItem(id, title));
+    });
+  }
+  return items;
 }
